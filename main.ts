0484c91f85514d138986741b193cfea9bc1be042
Ivan <39419547+ivanjtr@users.noreply.github.com>
2023-11-14 18:35:09 +0000

    musica

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
     music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 })
